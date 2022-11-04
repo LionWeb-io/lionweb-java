75e48def201071beb5e66bbe891e9bd2677678eb
Federico Tomassetti <federico@tomassetti.me>
2022-11-04 14:09:01 +0100

    revising javadoc comments to consider the case of Derived features

--- lib/src/main/java/org/lionweb/lioncore/java/model/HasFeatureValues.java
+++ lib/src/main/java/org/lionweb/lioncore/java/model/HasFeatureValues.java
@@ -11,6 +11,12 @@ public interface HasFeatureValues {
      * Get the property value associated with the specified property.
      */
     Object getPropertyValue(Property property);
+
+    /**
+     * If the value is not compatible with the type of the property, the exception IllegalArgumentException will be
+     * thrown.
+     * If the feature is derived, the exception IllegalArgumentException will be thrown.
+     */
     void setPropertyValue(Property property, Object value);
 
     /**
@@ -27,12 +33,19 @@ public interface HasFeatureValues {
      * Add a child to the specified list of children associated with the given Containment relation.
      * If the specified Containment does not allow for multiple values, and if a value is already set than
      * the exception IllegalStateException will be thrown.
+     *
+     * If the child has not a Concept compatible with the target of the Containement, the exception
+     * IllegalArgumentException will be
+     * thrown.
+     * If the Containment feature is derived, the exception IllegalArgumentException will be thrown.
      */
-    void addChild(Containment containment);
+    void addChild(Containment containment, Node child);
 
     /**
      * Remove the given child from the list of children associated with the Node, making it a dangling Node.
      * If the specified Node is not currently a child of this Node the exception IllegalArgumentException will be thrown.
+     *
+     * If the Containment feature is derived, the exception IllegalArgumentException will be thrown.
      */
     void removeChild(Node node);
 
@@ -51,6 +64,10 @@ public interface HasFeatureValues {
      *
      * The Node specified should be either part of this Node's Model or of Models imported by this Node's
      * Model. If that is not the case the exception IllegalArgumentException will be thrown.
+     *
+     * If the referredNode has not a Concept compatible with the target of the Reference, the exception
+     * IllegalArgumentException will be thrown.
+     * If the Reference feature is derived, the exception IllegalArgumentException will be thrown.
      */
     void setReferredNode(Reference reference, Node referredNode);
 }
