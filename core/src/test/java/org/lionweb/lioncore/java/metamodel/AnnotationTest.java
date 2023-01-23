4526e429864b814c67153bfe1c97070e9e9c8306
Federico Tomassetti <federico@tomassetti.me>
2023-01-23 15:57:44 +0100

    remove unused tests

--- core/src/test/java/org/lionweb/lioncore/java/metamodel/AnnotationTest.java
+++ core/src/test/java/org/lionweb/lioncore/java/metamodel/AnnotationTest.java
@@ -14,25 +14,6 @@ import static org.junit.Assert.assertTrue;
 public class AnnotationTest {
 
     @Test
-    public void anEmptyAnnotationIsInvalid() {
-        Annotation annotation = new Annotation();
-        assertFalse(annotation.validate().isSuccessful());
-        assertFalse(annotation.isValid());
-        assertEquals(2, annotation.validate().getIssues().size());
-        assertTrue(annotation.validate().getIssues().stream().allMatch(issue -> issue.isError()));
-        assertEquals(new HashSet<>(Arrays.asList("Metamodel not set", "Simple name not set")), annotation.validate().getIssues().stream().map(issue -> issue.getMessage()).collect(Collectors.toSet()));
-    }
-
-    @Test
-    public void anAnnotationCanBeValid() {
-        Metamodel metamodel = new Metamodel();
-        Annotation annotation = new Annotation(metamodel, "MyAnnotation");
-        assertTrue(annotation.validate().isSuccessful());
-        assertTrue(annotation.isValid());
-        assertEquals(0, annotation.validate().getIssues().size());
-    }
-
-    @Test
     public void getPropertyValueSimpleName() {
         System.out.println(LionCore.getAnnotation().allFeatures());
         Metamodel metamodel = new Metamodel();
