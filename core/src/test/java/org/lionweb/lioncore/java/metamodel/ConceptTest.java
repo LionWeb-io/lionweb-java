b4bb9f585d7b2d3e10409dea95db7d440dafaa2e
Federico Tomassetti <federico@tomassetti.me>
2023-04-26 14:51:55 +0200

    formatting

--- core/src/test/java/org/lionweb/lioncore/java/metamodel/ConceptTest.java
+++ core/src/test/java/org/lionweb/lioncore/java/metamodel/ConceptTest.java
@@ -1,14 +1,14 @@
 package org.lionweb.lioncore.java.metamodel;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class ConceptTest {
 
-    @Test
-    public void aConceptIsNonAbstractByDefault() {
-        Concept  c = new Concept();
-        assertEquals(false, c.isAbstract());
-    }
+  @Test
+  public void aConceptIsNonAbstractByDefault() {
+    Concept c = new Concept();
+    assertEquals(false, c.isAbstract());
+  }
 }
