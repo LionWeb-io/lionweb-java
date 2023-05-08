df92f863c2652d2c9fed40ca6896a6b62990c6bb
Federico Tomassetti <federico@strumenta.com>
2023-05-08 14:57:32 +0200

    initial draft of the EcoreExporter

--- emf/src/main/java/io/lionweb/lioncore/java/emf/EcoreExporter.java
+++ emf/src/main/java/io/lionweb/lioncore/java/emf/EcoreExporter.java
@@ -1,16 +1,20 @@
 package io.lionweb.lioncore.java.emf;
 
-import io.lionweb.lioncore.java.metamodel.Concept;
-import io.lionweb.lioncore.java.metamodel.Metamodel;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EcoreFactory;
+import io.lionweb.lioncore.java.metamodel.*;
+import org.eclipse.emf.ecore.*;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceImpl;
 
 import java.util.List;
 
 public class EcoreExporter {
     public Resource exportResource(List<Metamodel> metamodels) {
+        Resource resource = new ResourceImpl();
+        metamodels.forEach(m -> resource.getContents().add(exportMetamodel(m)));
+        return resource;
+    }
+
+    private EDataType toEDataType(DataType dataType) {
         throw new UnsupportedOperationException();
     }
 
@@ -27,6 +31,33 @@ public class EcoreExporter {
 
                 EClass eClass = EcoreFactory.eINSTANCE.createEClass();
                 eClass.setName(concept.getName());
+
+                concept.getFeatures().forEach(f ->  {
+                   if (f instanceof Property) {
+                       Property property = (Property)f;
+
+                       EAttribute eAttribute = EcoreFactory.eINSTANCE.createEAttribute();
+                       eAttribute.setName(property.getName());
+                       if (property.isOptional()) {
+                           eAttribute.setLowerBound(0);
+                       } else {
+                           eAttribute.setLowerBound(1);
+                       }
+                       eAttribute.setUpperBound(1);
+                       eAttribute.setEType(toEDataType(property.getType()));
+                       eClass.getEStructuralFeatures().add(eAttribute);
+                   } else if (f instanceof Containment) {
+                       EReference eReference = EcoreFactory.eINSTANCE.createEReference();
+                       eReference.setContainment(true);
+                       eClass.getEStructuralFeatures().add(eReference);
+                   } else if (f instanceof Reference) {
+                       EReference eReference = EcoreFactory.eINSTANCE.createEReference();
+                       eReference.setContainment(false);
+                       eClass.getEStructuralFeatures().add(eReference);
+                   } else {
+                       throw new IllegalStateException();
+                   }
+                });
 
                 ePackage.getEClassifiers().add(eClass);
             } else {
--- emf/src/test/java/io/lionweb/lioncore/java/emf/EcoreExporterTest.java
+++ emf/src/test/java/io/lionweb/lioncore/java/emf/EcoreExporterTest.java
@@ -2,8 +2,7 @@ package io.lionweb.lioncore.java.emf;
 
 import io.lionweb.lioncore.java.metamodel.*;
 import io.lionweb.lioncore.java.serialization.JsonSerialization;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.*;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -29,11 +28,35 @@ public class EcoreExporterTest {
     assertEquals("library", libraryPkg.getNsPrefix());
     assertEquals(5, libraryPkg.getEClassifiers().size());
 
+    EClass writer = (EClass) libraryPkg.getEClassifiers().stream().filter(e -> e.getName().equals("Writer")).findFirst().get();
+
     EClass book = (EClass) libraryPkg.getEClassifiers().stream().filter(e -> e.getName().equals("Book")).findFirst().get();
+    assertEquals("Book", book.getName());
+    assertEquals(false, book.isAbstract());
+    assertEquals(false, book.isInterface());
+    assertEquals(3, book.getEStructuralFeatures().size());
+
+    EAttribute bookTitle = (EAttribute)book.getEStructuralFeature("title");
+    assertEquals("title", bookTitle.getName());
+    assertEquals(1, bookTitle.getLowerBound());
+    assertEquals(1, bookTitle.getUpperBound());
+    assertEquals(EcorePackage.eINSTANCE.getEString(), bookTitle.getEType());
+
+    EAttribute bookPages = (EAttribute)book.getEStructuralFeature("pages");
+    assertEquals("pages", bookPages.getName());
+    assertEquals(1, bookPages.getLowerBound());
+    assertEquals(1, bookPages.getUpperBound());
+    assertEquals(EcorePackage.eINSTANCE.getEInt(), bookPages.getEType());
+
+    EReference bookAuthor = (EReference)book.getEStructuralFeature("author");
+    assertEquals("author", bookAuthor.getName());
+    assertEquals(false, bookAuthor.isContainment());
+    assertEquals(1, bookPages.getLowerBound());
+    assertEquals(-1, bookPages.getUpperBound());
+    assertEquals(writer, bookPages.getEType());
 
     EClass library = (EClass) libraryPkg.getEClassifiers().stream().filter(e -> e.getName().equals("Library")).findFirst().get();
 
-    EClass writer = (EClass) libraryPkg.getEClassifiers().stream().filter(e -> e.getName().equals("Writer")).findFirst().get();
 
     EClass specialistBookWriter = (EClass) libraryPkg.getEClassifiers().stream().filter(e -> e.getName().equals("SpecialistBookWriter")).findFirst().get();
 
