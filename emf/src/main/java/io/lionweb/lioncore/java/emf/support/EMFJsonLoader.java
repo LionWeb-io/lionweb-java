11ed5faf4d3108141dfb3035a4f10a0f3440f152
Federico Tomassetti <federico@strumenta.com>
2023-05-08 18:18:42 +0200

    wip

--- emf/src/main/java/io/lionweb/lioncore/java/emf/support/EMFJsonLoader.java
+++ emf/src/main/java/io/lionweb/lioncore/java/emf/support/EMFJsonLoader.java
@@ -1,12 +1,15 @@
 package io.lionweb.lioncore.java.emf.support;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.regex.Pattern;
 
+import netscape.javascript.JSObject;
 import org.eclipse.emf.ecore.*;
 import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -16,16 +19,19 @@ import org.eclipse.emf.ecore.resource.Resource;
  * implementation of the logic to load Resources stored as JSON.
  */
 public class EMFJsonLoader {
+
+  private JsonElement jsonRoot;
+
   public List<EObject> load(InputStream inputStream, Resource resource) {
-    JsonElement json = JsonParser.parseReader(new InputStreamReader(inputStream));
-    if (json.isJsonObject()) {
+    jsonRoot = JsonParser.parseReader(new InputStreamReader(inputStream));
+    if (jsonRoot.isJsonObject()) {
       List<EObject> nodes =
-          Collections.singletonList(readEObject(json.getAsJsonObject(), resource, null));
+          Collections.singletonList(readEObject(jsonRoot.getAsJsonObject(), resource, null));
       resource.getContents().addAll(nodes);
       return nodes;
-    } else if (json.isJsonArray()) {
+    } else if (jsonRoot.isJsonArray()) {
       List<EObject> nodes = new ArrayList<>();
-      json.getAsJsonArray()
+      jsonRoot.getAsJsonArray()
           .forEach(
               jsonElement -> nodes.add(readEObject(jsonElement.getAsJsonObject(), resource, null)));
       resource.getContents().addAll(nodes);
@@ -35,10 +41,33 @@ public class EMFJsonLoader {
     }
   }
 
-  private EObject resolveReference(String ref, Resource resource) {
+  private JsonElement navigateJson(JsonElement container, String path) {
+    throw new UnsupportedOperationException();
+  }
+
+  private EObject resolveReference(JsonElement container, String path) {
+    if (path.startsWith("/")) {
+      path = path.substring(1);
+    }
+    String[] parts = path.split("/");
+    if (parts.length == 1) {
+      Pattern digits = Pattern.compile("\\d+");
+      if (digits.matcher(parts[0]).matches()) {
+        //return resource.getContents().get(Integer.parseInt(parts[0]));
+        throw new UnsupportedOperationException();
+      } else {
+        throw new UnsupportedOperationException();
+      }
+    } else {
+      JsonElement nextContainer = navigateJson(jsonRoot, parts[0]);
+      return resolveReference(nextContainer, path.substring(("/" + parts[0]).length()));
+    }
+  }
+
+  private EObject resolveReference(String ref, EPackage.Registry packagesRegistry) {
     String[] parts = ref.split("#//");
     if (parts.length == 2) {
-      EPackage ePackage = resource.getResourceSet().getPackageRegistry().getEPackage(parts[0]);
+      EPackage ePackage = packagesRegistry.getEPackage(parts[0]);
       if (ePackage == null) {
         throw new IllegalStateException("Unable to resolve package " + parts[0]);
       } else {
@@ -53,7 +82,9 @@ public class EMFJsonLoader {
           }
         }
       }
-
+    } else if (parts.length == 1) {
+      String[] subparts = parts[0].split("/");
+      return resolveReference(parts[0]);
     } else {
       throw new UnsupportedOperationException();
     }
@@ -144,7 +175,7 @@ public class EMFJsonLoader {
                     } else {
                       if (jsonFeatureValue.isJsonObject()) {
                         String ref = jsonFeatureValue.getAsJsonObject().get("$ref").getAsString();
-                        EObject referred = resolveReference(ref, resource);
+                        EObject referred = resolveReference(ref, resource.getResourceSet().getPackageRegistry());
                         eObject.eSet(eStructuralFeature, referred);
                       } else {
                         throw new UnsupportedOperationException(
