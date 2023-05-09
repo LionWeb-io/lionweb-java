064520067c345d5eaf80a37161224f23b03d7165
Federico Tomassetti <federico@strumenta.com>
2023-05-09 11:30:10 +0200

    correct logic to resolve references in JsonResource

--- emf/src/main/java/io/lionweb/lioncore/java/emf/support/EMFJsonLoader.java
+++ emf/src/main/java/io/lionweb/lioncore/java/emf/support/EMFJsonLoader.java
@@ -1,17 +1,13 @@
 package io.lionweb.lioncore.java.emf.support;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.regex.Pattern;
-
-import netscape.javascript.JSObject;
+import java.util.stream.Collectors;
 import org.eclipse.emf.ecore.*;
-import org.eclipse.emf.ecore.impl.DynamicEObjectImpl;
 import org.eclipse.emf.ecore.resource.Resource;
 
 /**
@@ -20,77 +16,287 @@ import org.eclipse.emf.ecore.resource.Resource;
  */
 public class EMFJsonLoader {
 
-  private JsonElement jsonRoot;
+  // private JsonElement jsonRoot;
+
+  private class ReferencePostponer {
+
+    private EPackage.Registry packagesRegistry;
+
+    public ReferencePostponer(EPackage.Registry packagesRegistry) {
+      this.packagesRegistry = packagesRegistry;
+    }
+
+    private class PostponedReference {
+      EObject container;
+      EStructuralFeature eStructuralFeature;
+      List<String> refs;
+
+      public PostponedReference(
+          EObject container, EStructuralFeature eStructuralFeature, String ref) {
+        this.container = container;
+        this.eStructuralFeature = eStructuralFeature;
+        this.refs = Collections.singletonList(ref);
+      }
+
+      public PostponedReference(
+          EObject container, EStructuralFeature eStructuralFeature, List<String> refs) {
+        this.container = container;
+        this.eStructuralFeature = eStructuralFeature;
+        this.refs = refs;
+      }
+    }
+
+    private List<PostponedReference> postponedReferences = new ArrayList<>();
+
+    public void considerReferences(List<EObject> theseNodes) {
+      postponedReferences.forEach(
+          postponedReference -> {
+            if (postponedReference.eStructuralFeature.isMany()) {
+              List<EObject> referred =
+                  postponedReference.refs.stream()
+                      .map(ref -> resolve(ref, theseNodes))
+                      .collect(Collectors.toList());
+              postponedReference.container.eSet(postponedReference.eStructuralFeature, referred);
+            } else {
+              if (postponedReference.refs.size() != 1) {
+                throw new IllegalStateException();
+              }
+              EObject referred = resolve(postponedReference.refs.get(0), theseNodes);
+              postponedReference.container.eSet(postponedReference.eStructuralFeature, referred);
+            }
+          });
+    }
+
+    private EObject resolve(String ref, List<EObject> theseNodes) {
+      String[] parts = ref.split("#/");
+      if (parts.length == 2 && packagesRegistry.containsKey(parts[0])) {
+        EPackage ePackage = packagesRegistry.getEPackage(parts[0]);
+        return resolve(ePackage, parts[1], theseNodes);
+      } else if (ref.startsWith("/")) {
+        String[] subparts = ref.substring(1).split("/");
+        int index = Integer.parseInt(subparts[0]);
+        EObject root = theseNodes.get(index);
+        if (subparts.length == 1) {
+          return root;
+        } else {
+          return resolve(root, ref.substring(("/" + subparts[0]).length()), theseNodes);
+        }
+      } else {
+        throw new UnsupportedOperationException();
+      }
+    }
+
+    private EObject resolve(EObject container, String path, List<EObject> theseNodes) {
+      if (!path.startsWith("/")) {
+        throw new IllegalStateException();
+      }
+      String[] parts = path.substring(1).split("/");
+      if (parts.length == 1) {
+        List<EObject> matching =
+            container.eContents().stream()
+                .filter(
+                    child -> {
+                      EStructuralFeature nameFeature = child.eClass().getEStructuralFeature("name");
+                      if (nameFeature != null) {
+                        return Objects.equals(child.eGet(nameFeature), path.substring(1));
+                      } else {
+                        return false;
+                      }
+                    })
+                .collect(Collectors.toList());
+        if (matching.size() != 1) {
+          throw new IllegalStateException(
+              "Matching " + matching + " for path " + path + " in " + container);
+        }
+        return matching.get(0);
+      } else {
+        String firstElementPath = "/" + parts[0];
+        EObject nextContainer = resolve(container, firstElementPath, theseNodes);
+        String nextPath = path.substring(firstElementPath.length());
+        return resolve(nextContainer, nextPath, theseNodes);
+      }
+    }
+
+    public void recordReference(
+        EObject container, EStructuralFeature eStructuralFeature, String ref) {
+      postponedReferences.add(new PostponedReference(container, eStructuralFeature, ref));
+    }
+
+    public void recordReference(
+        EObject container, EStructuralFeature eStructuralFeature, List<String> refs) {
+      postponedReferences.add(new PostponedReference(container, eStructuralFeature, refs));
+    }
+  }
 
   public List<EObject> load(InputStream inputStream, Resource resource) {
-    jsonRoot = JsonParser.parseReader(new InputStreamReader(inputStream));
+    JsonElement jsonRoot = JsonParser.parseReader(new InputStreamReader(inputStream));
+    ReferencePostponer referencePostponer =
+        new ReferencePostponer(resource.getResourceSet().getPackageRegistry());
+    List<EObject> nodes;
     if (jsonRoot.isJsonObject()) {
-      List<EObject> nodes =
-          Collections.singletonList(readEObject(jsonRoot.getAsJsonObject(), resource, null));
-      resource.getContents().addAll(nodes);
-      return nodes;
+      nodes =
+          Collections.singletonList(
+              readEObject(
+                  jsonRoot.getAsJsonObject(),
+                  null,
+                  resource.getResourceSet().getPackageRegistry(),
+                  referencePostponer));
     } else if (jsonRoot.isJsonArray()) {
-      List<EObject> nodes = new ArrayList<>();
-      jsonRoot.getAsJsonArray()
+      nodes = new ArrayList<>();
+      jsonRoot
+          .getAsJsonArray()
           .forEach(
-              jsonElement -> nodes.add(readEObject(jsonElement.getAsJsonObject(), resource, null)));
-      resource.getContents().addAll(nodes);
-      return nodes;
+              jsonElement ->
+                  nodes.add(
+                      readEObject(
+                          jsonElement.getAsJsonObject(),
+                          null,
+                          resource.getResourceSet().getPackageRegistry(),
+                          referencePostponer)));
+    } else {
+      throw new UnsupportedOperationException();
+    }
+    resource.getContents().addAll(nodes);
+    referencePostponer.considerReferences(nodes);
+    return nodes;
+  }
+
+  //  private JsonElement navigateJson(JsonElement container, String path) {
+  //    throw new UnsupportedOperationException();
+  //  }
+  //
+  //  private EObject resolveReference(JsonElement container, String path) {
+  //    if (path.startsWith("/")) {
+  //      path = path.substring(1);
+  //    }
+  //    String[] parts = path.split("/");
+  //    if (parts.length == 1) {
+  //      Pattern digits = Pattern.compile("\\d+");
+  //      if (digits.matcher(parts[0]).matches()) {
+  //        //return resource.getContents().get(Integer.parseInt(parts[0]));
+  //        throw new UnsupportedOperationException();
+  //      } else {
+  //        throw new UnsupportedOperationException();
+  //      }
+  //    } else {
+  //      JsonElement nextContainer = navigateJson(jsonRoot, parts[0]);
+  //      return resolveReference(nextContainer, path.substring(("/" + parts[0]).length()));
+  //    }
+  //  }
+  //
+  //  private EObject resolveReference(String ref, EPackage.Registry packagesRegistry) {
+  //    String[] parts = ref.split("#//");
+  //    if (parts.length == 2) {
+  //      EPackage ePackage = packagesRegistry.getEPackage(parts[0]);
+  //      if (ePackage == null) {
+  //        throw new IllegalStateException("Unable to resolve package " + parts[0]);
+  //      } else {
+  //        if (parts[1].contains("/")) {
+  //          throw new UnsupportedOperationException();
+  //        } else {
+  //          Optional<EClassifier> resolved = ePackage.getEClassifiers().stream().filter(e ->
+  // e.getName().equals(parts[1])).findFirst();
+  //          if (resolved.isPresent()) {
+  //            return resolved.get();
+  //          } else {
+  //            throw new IllegalStateException("Cannot find " + parts[1] + " in package " +
+  // ePackage);
+  //          }
+  //        }
+  //      }
+  //    } else if (parts.length == 1) {
+  //      String[] subparts = parts[0].split("/");
+  //      return resolveReference(parts[0]);
+  //    } else {
+  //      throw new UnsupportedOperationException();
+  //    }
+  //  }
+
+  private void readEAttribute(
+      EObject eObject, EAttribute eAttribute, JsonElement jsonFeatureValue) {
+    if (eAttribute.getEAttributeType().equals(EcorePackage.eINSTANCE.getEInt())) {
+      if (eAttribute.isMany()) {
+        throw new UnsupportedOperationException();
+      } else {
+        eObject.eSet(eAttribute, Integer.parseInt(jsonFeatureValue.getAsString()));
+      }
+    } else if (eAttribute.getEAttributeType().equals(EcorePackage.eINSTANCE.getEString())) {
+      if (eAttribute.isMany()) {
+        throw new UnsupportedOperationException();
+      } else {
+        eObject.eSet(eAttribute, jsonFeatureValue.getAsString());
+      }
+    } else if (eAttribute.getEAttributeType().equals(EcorePackage.eINSTANCE.getEBoolean())) {
+      if (eAttribute.isMany()) {
+        throw new UnsupportedOperationException();
+      } else {
+        eObject.eSet(eAttribute, Boolean.parseBoolean(jsonFeatureValue.getAsString()));
+      }
     } else {
       throw new UnsupportedOperationException();
     }
   }
 
-  private JsonElement navigateJson(JsonElement container, String path) {
-    throw new UnsupportedOperationException();
-  }
-
-  private EObject resolveReference(JsonElement container, String path) {
-    if (path.startsWith("/")) {
-      path = path.substring(1);
-    }
-    String[] parts = path.split("/");
-    if (parts.length == 1) {
-      Pattern digits = Pattern.compile("\\d+");
-      if (digits.matcher(parts[0]).matches()) {
-        //return resource.getContents().get(Integer.parseInt(parts[0]));
-        throw new UnsupportedOperationException();
-      } else {
-        throw new UnsupportedOperationException();
-      }
+  private void readEReferenceContainment(
+      EObject eObject,
+      EReference eReference,
+      JsonElement jsonFeatureValue,
+      EPackage.Registry packagesRegistry,
+      ReferencePostponer referencePostponer) {
+    if (eReference.isMany()) {
+      List<EObject> children = new ArrayList<>();
+      jsonFeatureValue
+          .getAsJsonArray()
+          .forEach(
+              e ->
+                  children.add(
+                      readEObject(
+                          e.getAsJsonObject(),
+                          eReference.getEReferenceType(),
+                          packagesRegistry,
+                          referencePostponer)));
+      eObject.eSet(eReference, children);
     } else {
-      JsonElement nextContainer = navigateJson(jsonRoot, parts[0]);
-      return resolveReference(nextContainer, path.substring(("/" + parts[0]).length()));
+      EObject child =
+          readEObject(
+              jsonFeatureValue.getAsJsonObject(),
+              eReference.getEReferenceType(),
+              packagesRegistry,
+              referencePostponer);
+      eObject.eSet(eReference, child);
     }
   }
 
-  private EObject resolveReference(String ref, EPackage.Registry packagesRegistry) {
-    String[] parts = ref.split("#//");
-    if (parts.length == 2) {
-      EPackage ePackage = packagesRegistry.getEPackage(parts[0]);
-      if (ePackage == null) {
-        throw new IllegalStateException("Unable to resolve package " + parts[0]);
-      } else {
-        if (parts[1].contains("/")) {
-          throw new UnsupportedOperationException();
-        } else {
-          Optional<EClassifier> resolved = ePackage.getEClassifiers().stream().filter(e -> e.getName().equals(parts[1])).findFirst();
-          if (resolved.isPresent()) {
-            return resolved.get();
-          } else {
-            throw new IllegalStateException("Cannot find " + parts[1] + " in package " + ePackage);
-          }
-        }
-      }
-    } else if (parts.length == 1) {
-      String[] subparts = parts[0].split("/");
-      return resolveReference(parts[0]);
+  private void readEReferenceNonContainment(
+      EObject eObject,
+      EReference eReference,
+      JsonElement jsonFeatureValue,
+      EPackage.Registry packagesRegistry,
+      ReferencePostponer referencePostponer) {
+    if (eReference.isMany()) {
+      List<String> refs = new ArrayList<>();
+      jsonFeatureValue
+          .getAsJsonArray()
+          .forEach(e -> refs.add(e.getAsJsonObject().get("$ref").getAsString()));
+      referencePostponer.recordReference(eObject, eReference, refs);
     } else {
-      throw new UnsupportedOperationException();
+      if (jsonFeatureValue.isJsonObject()) {
+        String ref = jsonFeatureValue.getAsJsonObject().get("$ref").getAsString();
+        referencePostponer.recordReference(eObject, eReference, ref);
+        // EObject referred = resolveReference(ref, resource.getResourceSet().getPackageRegistry());
+        // eObject.eSet(eStructuralFeature, referred);
+      } else {
+        throw new UnsupportedOperationException(
+            "Non-containment EReferences are not yet supported");
+      }
     }
   }
 
-  private EObject readEObject(JsonObject jsonObject, Resource resource, EClass expectedEClass) {
+  private EObject readEObject(
+      JsonObject jsonObject,
+      EClass expectedEClass,
+      EPackage.Registry packagesRegistry,
+      ReferencePostponer referencePostponer) {
     Objects.requireNonNull(jsonObject);
     String eClassURI = null;
     if (jsonObject.has("eClass")) {
@@ -106,7 +312,7 @@ public class EMFJsonLoader {
       String[] parts = eClassURI.split("#//");
       String packageURI = parts[0];
       String eClassName = parts[1];
-      EPackage ePackage = resource.getResourceSet().getPackageRegistry().getEPackage(packageURI);
+      EPackage ePackage = packagesRegistry.getEPackage(packageURI);
       if (ePackage == null) {
         throw new UnsupportedOperationException();
       }
@@ -127,61 +333,23 @@ public class EMFJsonLoader {
               JsonElement jsonFeatureValue = jsonObject.get(eStructuralFeature.getName());
               if (jsonFeatureValue != null) {
                 if (eStructuralFeature instanceof EAttribute) {
-                  EAttribute eAttribute = (EAttribute) eStructuralFeature;
-                  if (eAttribute.getEAttributeType().equals(EcorePackage.eINSTANCE.getEInt())) {
-                    if (eStructuralFeature.isMany()) {
-                      throw new UnsupportedOperationException();
-                    } else {
-                      eObject.eSet(
-                          eStructuralFeature, Integer.parseInt(jsonFeatureValue.getAsString()));
-                    }
-                  } else if (eAttribute
-                      .getEAttributeType()
-                      .equals(EcorePackage.eINSTANCE.getEString())) {
-                    if (eStructuralFeature.isMany()) {
-                      throw new UnsupportedOperationException();
-                    } else {
-                      eObject.eSet(eStructuralFeature, jsonFeatureValue.getAsString());
-                    }
-                  } else {
-                    throw new UnsupportedOperationException();
-                  }
+                  readEAttribute(eObject, (EAttribute) eStructuralFeature, jsonFeatureValue);
                 } else if (eStructuralFeature instanceof EReference) {
                   EReference eReference = (EReference) eStructuralFeature;
                   if (eReference.isContainment()) {
-                    if (eStructuralFeature.isMany()) {
-                      List<EObject> children = new ArrayList<>();
-                      jsonFeatureValue
-                          .getAsJsonArray()
-                          .forEach(
-                              e ->
-                                  children.add(
-                                      readEObject(
-                                          e.getAsJsonObject(),
-                                          resource,
-                                          ((EReference) eStructuralFeature).getEReferenceType())));
-                      eObject.eSet(eStructuralFeature, children);
-                    } else {
-                      EObject child =
-                          readEObject(
-                              jsonFeatureValue.getAsJsonObject(),
-                              resource,
-                              ((EReference) eStructuralFeature).getEReferenceType());
-                      eObject.eSet(eStructuralFeature, child);
-                    }
+                    readEReferenceContainment(
+                        eObject,
+                        eReference,
+                        jsonFeatureValue,
+                        packagesRegistry,
+                        referencePostponer);
                   } else {
-                    if (eStructuralFeature.isMany()) {
-                      throw new UnsupportedOperationException();
-                    } else {
-                      if (jsonFeatureValue.isJsonObject()) {
-                        String ref = jsonFeatureValue.getAsJsonObject().get("$ref").getAsString();
-                        EObject referred = resolveReference(ref, resource.getResourceSet().getPackageRegistry());
-                        eObject.eSet(eStructuralFeature, referred);
-                      } else {
-                        throw new UnsupportedOperationException(
-                                "Non-containment EReferences are not yet supported");
-                      }
-                    }
+                    readEReferenceNonContainment(
+                        eObject,
+                        eReference,
+                        jsonFeatureValue,
+                        packagesRegistry,
+                        referencePostponer);
                   }
                 } else {
                   throw new IllegalStateException();
