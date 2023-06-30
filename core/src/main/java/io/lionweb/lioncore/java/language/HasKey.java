744a122d0f44c36ea3a274b95b5fb9ef929923b3
Federico Tomassetti <federico@strumenta.com>
2023-06-30 13:24:22 +0200

    linting

--- core/src/main/java/io/lionweb/lioncore/java/language/HasKey.java
+++ core/src/main/java/io/lionweb/lioncore/java/language/HasKey.java
@@ -2,6 +2,7 @@ package io.lionweb.lioncore.java.language;
 
 /**
  * Implementation detail of the Java implementation. Not part of the official M3.
+ *
  * @param <T>
  */
 public interface HasKey<T> {
