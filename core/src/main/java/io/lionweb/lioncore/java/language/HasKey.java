4d4092bbd050fc148d0148c96733228b38cde767
Federico Tomassetti <federico@strumenta.com>
2023-06-30 13:23:55 +0200

    adding comment on HasKey

--- core/src/main/java/io/lionweb/lioncore/java/language/HasKey.java
+++ core/src/main/java/io/lionweb/lioncore/java/language/HasKey.java
@@ -1,5 +1,9 @@
 package io.lionweb.lioncore.java.language;
 
+/**
+ * Implementation detail of the Java implementation. Not part of the official M3.
+ * @param <T>
+ */
 public interface HasKey<T> {
   String getKey();
 
