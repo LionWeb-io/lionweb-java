4f9c94e607b90af4388cd50e8ad2ff4cc5c7ae57
Niko Stotz <git@nikostotz.de>
2023-08-08 18:03:54 +0200

    added unittests for other integration test examples

---
+++ core/src/test/java/io/lionweb/lioncore/java/testset/ValidWithLanguage.java
@@ -0,0 +1,31 @@
+package io.lionweb.lioncore.java.testset;
+
+import static org.junit.Assert.assertFalse;
+
+import io.lionweb.lioncore.java.model.Node;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class ValidWithLanguage extends ALanguageTestset {
+  @Parameterized.Parameters
+  public static Object[] inputFiles() {
+    Path integrationTests = findIntegrationTests();
+    Path basePath = integrationTests.resolve("testset").resolve("withLanguage").resolve("valid");
+    Object[] result = collectJsonFiles(basePath);
+    return result;
+  }
+
+  public ValidWithLanguage(Path path) {
+    super(path);
+  }
+
+  @Test
+  public void assertValid() {
+    List<Node> nodes = parse(path, getSerialization());
+    assertFalse(path.toString(), nodes.isEmpty());
+  }
+}
