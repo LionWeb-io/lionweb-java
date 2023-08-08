738d92587567002cf33729b6a549d6fea267b257
Niko Stotz <git@nikostotz.de>
2023-08-08 14:01:30 +0200

    fixed formatting

--- core/src/test/java/io/lionweb/lioncore/java/testset/Invalid.java
+++ core/src/test/java/io/lionweb/lioncore/java/testset/Invalid.java
@@ -1,54 +1,58 @@
 package io.lionweb.lioncore.java.testset;
 
+import static org.junit.Assert.assertThrows;
+
 import io.lionweb.lioncore.java.model.Node;
 import io.lionweb.lioncore.java.serialization.JsonSerialization;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-
-import static org.junit.Assert.assertThrows;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
 public class Invalid {
-    private final Path path;
+  private final Path path;
 
-    @Parameterized.Parameters
-    public static Object[] inputFiles() {
-        Path integrationTests = findIntegrationTests();
-        Path basePath = integrationTests.resolve("testset").resolve("invalid");
-        try {
-            Object[] result = Files.walk(basePath)
-                    .filter(Files::isRegularFile)
-                    .filter(f -> f.toString().endsWith(".json")
-                    ).toArray();
-            return result;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+  @Parameterized.Parameters
+  public static Object[] inputFiles() {
+    Path integrationTests = findIntegrationTests();
+    Path basePath = integrationTests.resolve("testset").resolve("invalid");
+    try {
+      Object[] result =
+          Files.walk(basePath)
+              .filter(Files::isRegularFile)
+              .filter(f -> f.toString().endsWith(".json"))
+              .toArray();
+      return result;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public Invalid(Path path) {
-        this.path = path;
-    }
+  public Invalid(Path path) {
+    this.path = path;
+  }
 
-    @Test
-    public void assertInvalid() {
-        assertThrows(path.toString(), RuntimeException.class, () -> {
-            List<Node> nodes = JsonSerialization.getStandardSerialization().unserializeToNodes(path.toFile());
-            System.out.println(nodes);
+  @Test
+  public void assertInvalid() {
+    assertThrows(
+        path.toString(),
+        RuntimeException.class,
+        () -> {
+          List<Node> nodes =
+              JsonSerialization.getStandardSerialization().unserializeToNodes(path.toFile());
+          System.out.println(nodes);
         });
-    }
+  }
 
-    private static Path findIntegrationTests() {
-        String propertyValue = System.getenv("integrationTestingDir");
-        if (propertyValue != null) {
-            return Path.of(propertyValue);
-        }
-        throw new IllegalArgumentException("environment variable integrationTestingDir not defined.");
+  private static Path findIntegrationTests() {
+    String propertyValue = System.getenv("integrationTestingDir");
+    if (propertyValue != null) {
+      return Path.of(propertyValue);
     }
+    throw new IllegalArgumentException("environment variable integrationTestingDir not defined.");
+  }
 }
