d6a270fe7d338b5cac4391530f8a1987193031e7
Niko Stotz <niko@nikostotz.de>
2023-11-20 21:59:18 +0100

    shut up JavaDoc warnings

--- core/src/main/java/io/lionweb/lioncore/java/language/IKeyed.java
+++ core/src/main/java/io/lionweb/lioncore/java/language/IKeyed.java
@@ -2,8 +2,9 @@ package io.lionweb.lioncore.java.language;
 
 /**
  * Implementation detail of the Java implementation. Not part of the official M3.
+ * TODO: Is this statement still correct?
  *
- * @param <T>
+ * @param <T> Type of keyed element.
  */
 public interface IKeyed<T> extends INamed {
   String getKey();
