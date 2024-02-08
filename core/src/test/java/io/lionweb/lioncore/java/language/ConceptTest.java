356dbaf34eec8dbd02e4dd025bb361646c1f8e41
Federico Tomassetti <federico@strumenta.com>
2024-02-08 16:39:05 +0100

    Adding tests to check inheritance of duplicate features

--- core/src/test/java/io/lionweb/lioncore/java/language/ConceptTest.java
+++ core/src/test/java/io/lionweb/lioncore/java/language/ConceptTest.java
@@ -11,4 +11,45 @@ public class ConceptTest {
     Concept c = new Concept();
     assertEquals(false, c.isAbstract());
   }
+
+  @Test
+  public void featuresAndInheritanceCycles() {
+    Language l = new Language("MyLanguage", "l-id", "l-key", "123");
+    Concept a = new Concept(l, "A", "a-id", "a-key");
+    Concept b = new Concept(l, "B", "b-id", "b-key");
+    Concept c = new Concept(l, "C", "c-id", "c-key");
+    b.setExtendedConcept(a);
+    c.setExtendedConcept(b);
+    b.addFeature(new Property("P1", b, "p1-id").setKey("p1-key"));
+
+    assertEquals(0, a.allFeatures().size());
+    assertEquals(0, a.inheritedFeatures().size());
+    assertEquals(1, b.allFeatures().size());
+    assertEquals(0, b.inheritedFeatures().size());
+    assertEquals(1, c.allFeatures().size());
+    assertEquals(1, c.inheritedFeatures().size());
+  }
+
+  @Test
+  public void checkDuplicateInheritance() {
+    Language l = new Language("MyLanguage", "l-id", "l-key", "123");
+    Interface a = new Interface(l, "A", "a-id", "a-key");
+    Interface b = new Interface(l, "B", "b-id", "b-key");
+    Interface c = new Interface(l, "C", "c-id", "c-key");
+    Interface d = new Interface(l, "D", "d-id", "d-key");
+    b.addExtendedInterface(d);
+    c.addExtendedInterface(d);
+    a.addExtendedInterface(b);
+    a.addExtendedInterface(c);
+    d.addFeature(new Property("P1", d, "p1-id").setKey("p1-key"));
+
+    assertEquals(1, a.allFeatures().size());
+    assertEquals(1, a.inheritedFeatures().size());
+    assertEquals(1, b.allFeatures().size());
+    assertEquals(1, b.inheritedFeatures().size());
+    assertEquals(1, c.allFeatures().size());
+    assertEquals(1, c.inheritedFeatures().size());
+    assertEquals(1, d.allFeatures().size());
+    assertEquals(0, d.inheritedFeatures().size());
+  }
 }
