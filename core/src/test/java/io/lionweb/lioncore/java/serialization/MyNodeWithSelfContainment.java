1aa0cde7abce81268e9a4ab6a7aab63357f9aa8e
Federico Tomassetti <federico@strumenta.com>
2024-06-07 12:46:34 +0200

    Add DynamicNodeTest.testGetContainmentFeature

---
+++ core/src/test/java/io/lionweb/lioncore/java/serialization/MyNodeWithSelfContainment.java
@@ -0,0 +1,33 @@
+package io.lionweb.lioncore.java.serialization;
+
+import io.lionweb.lioncore.java.language.*;
+import io.lionweb.lioncore.java.model.impl.DynamicNode;
+
+public class MyNodeWithSelfContainment extends DynamicNode {
+  public static final Language LANGUAGE =
+      new Language().setID("mm3").setKey("mylanguage3").setName("MM3").setVersion("1");
+  public static final Concept CONCEPT =
+      new Concept()
+          .setID("concept-MyNodeWithSelfContainment")
+          .setKey("concept-MyNodeWithSelfContainment")
+          .setName("MyNodeWithSelfContainment")
+          .setParent(LANGUAGE);
+
+  static {
+    CONCEPT.addFeature(
+        Containment.createOptional("another", CONCEPT).setID("another-id").setKey("another-key"));
+    LANGUAGE.addElement(CONCEPT);
+  }
+
+  public MyNodeWithSelfContainment(String id) {
+    super(id, CONCEPT);
+  }
+
+  public MyNodeWithSelfContainment getAnother() {
+    return (MyNodeWithSelfContainment) this.getOnlyChildByContainmentName("another");
+  }
+
+  public void setAnother(MyNodeWithSelfContainment another) {
+    this.setOnlyChildByContainmentName("another", another);
+  }
+}
