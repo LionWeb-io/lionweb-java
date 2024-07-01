7bef6db5fa02ce0d059714e8daaebf1d10abfe8b
Federico Tomassetti <federico@strumenta.com>
2024-07-01 08:47:43 +0200

    correct documentation of ClassifierInstance.addAnnotation

--- core/src/main/java/io/lionweb/lioncore/java/model/ClassifierInstance.java
+++ core/src/main/java/io/lionweb/lioncore/java/model/ClassifierInstance.java
@@ -20,9 +20,8 @@ public interface ClassifierInstance<T extends Classifier<T>> extends HasFeatureV
   List<AnnotationInstance> getAnnotations(@Nonnull Annotation annotation);
 
   /**
-   * If an annotation instance was already associated under the Annotation link used by this
-   * AnnotationInstance, and the annotation does not support multiple values, then the existing
-   * instance will be removed and replaced by the instance specified in the call to this method.
+   * Add the annotation. Note that we can always have multiple annotation instances of the same annotation type
+   * attached to a node, so this will never replace an existing annotation instance.
    *
    * @throws IllegalArgumentException In case the specified Annotation link cannot be used on Nodes
    *     of this Concept.
