1b67409f8e3bb34136d3d83d85858a714fe53a65
Federico Tomassetti <federico@strumenta.com>
2025-04-09 08:19:39 +0200

    Setup publication of repo-client-testing

--- repo-client-testing/build.gradle.kts
+++ repo-client-testing/build.gradle.kts
@@ -1,10 +1,15 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     `jvm-test-suite`
     id("java-library")
     alias(libs.plugins.buildConfig)
+    id("signing")
+    id("com.vanniktech.maven.publish")
 }
 
 val jvmVersion = extra["jvmVersion"] as String
+val specsVersion = extra["specsVersion"] as String
 
 java {
     sourceCompatibility = JavaVersion.toVersion(jvmVersion)
@@ -44,4 +49,66 @@ buildConfig {
         buildConfigField("String", "LIONWEB_REPOSITORY_COMMIT_ID", "\"${lionwebRepositoryCommitID}\"")
         useJavaOutput()
     }
+}
+
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    // See https://discuss.gradle.org/t/why-subproject-sourceset-dirs-project-sourceset-dirs/7376/5
+    // Without the closure, parent sources are used for children too
+    from(sourceSets.getByName("main").java.srcDirs)
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.lionweb.lionweb-java",
+        artifactId = "lionweb-java-${specsVersion}-" + project.name,
+        version = project.version as String,
+    )
+
+    pom {
+        name.set("lionweb-java-" + project.name)
+        description.set("Java APIs for the LionWeb system")
+        version = project.version as String
+        packaging = "jar"
+        url.set("https://github.com/LionWeb-io/lionweb-java")
+
+        scm {
+            connection.set("scm:git:https://github.com/LionWeb-io/lionweb-java.git")
+            developerConnection.set("scm:git:git@github.com:LionWeb-io/lionweb-java.git")
+            url.set("https://github.com/LionWeb-io/lionweb-java.git")
+        }
+
+        licenses {
+            license {
+                name.set("Apache Licenve V2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                distribution.set("repo")
+            }
+        }
+
+        // The developers entry is strictly required by Maven Central
+        developers {
+            developer {
+                id.set("ftomassetti")
+                name.set("Federico Tomassetti")
+                email.set("federico@strumenta.com")
+            }
+            developer {
+                id.set("dslmeinte")
+                name.set("Meinte Boersma")
+                email.set("meinte.boersma@gmail.com")
+            }
+            developer {
+                id.set("enikao")
+                name.set("Niko Stotz")
+                email.set("github-public@nikostotz.de")
+            }
+        }
+    }
+    publishToMavenCentral(SonatypeHost.S01, true)
+    signAllPublications()
+}
+
+tasks.named("sourcesJar") {
+    dependsOn("generateBuildConfig")
 }
