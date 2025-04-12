b20149b49526ce2d9fefec82000389155ae48479
ftomassetti <federico@strumenta.com>
2025-04-12 07:33:06 +0200

    Launch containers once per test

--- repo-client-testing/src/main/java/io/lionweb/repoclient/testing/AbstractRepoClientFunctionalTest.java
+++ repo-client-testing/src/main/java/io/lionweb/repoclient/testing/AbstractRepoClientFunctionalTest.java
@@ -5,14 +5,14 @@ import io.lionweb.lioncore.java.model.Node;
 import io.lionweb.lioncore.java.utils.ModelComparator;
 import java.util.Collections;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AbstractRepoClientFunctionalTest {
   private static final int DB_CONTAINER_PORT = 5432;
 
@@ -21,6 +21,8 @@ public class AbstractRepoClientFunctionalTest {
 
   protected PostgreSQLContainer<?> db;
   protected GenericContainer<?> modelRepository;
+
+  protected static final Network network = Network.newNetwork();
 
   public AbstractRepoClientFunctionalTest() {
     this(LionWebVersion.currentVersion, true);
@@ -32,10 +34,8 @@ public class AbstractRepoClientFunctionalTest {
     this.modelRepoDebug = modelRepoDebug;
   }
 
-  @BeforeEach
+  @BeforeAll
   public void setup() {
-    Network network = Network.newNetwork();
-
     db =
         new PostgreSQLContainer<>("postgres:16.1")
             .withNetwork(network)
@@ -86,10 +86,13 @@ public class AbstractRepoClientFunctionalTest {
     System.setProperty("MODEL_REPO_PORT", Integer.toString(getModelRepoPort()));
   }
 
-  @AfterEach
-  public void teardown() {
+  @AfterAll
+  public void teardownContainers() {
     if (modelRepository != null) {
       modelRepository.stop();
+    }
+    if (db != null) {
+      db.stop();
     }
   }
 
