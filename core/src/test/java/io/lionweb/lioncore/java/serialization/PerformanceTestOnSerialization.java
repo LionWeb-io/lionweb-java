3d155f69a98467475bbccd5127d4bf8c66ce217b
Federico Tomassetti <federico@strumenta.com>
2025-04-16 18:05:43 +0200

    Add PerformanceTestOnSerialization

--- core/src/test/java/io/lionweb/lioncore/java/serialization/PerformanceTestOnSerialization.java
+++ core/src/test/java/io/lionweb/lioncore/java/serialization/PerformanceTestOnSerialization.java
@@ -1,54 +1,58 @@
 package io.lionweb.lioncore.java.serialization;
 
+import static org.junit.Assert.assertEquals;
+
 import io.lionweb.lioncore.java.LionWebVersion;
 import io.lionweb.lioncore.java.model.Node;
-import org.junit.Test;
-
 import java.io.*;
 import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class PerformanceTestOnSerialization {
 
-    @Test
-    public void deserializeLargeLanguage() {
-        InputStream is = this.getClass().getResourceAsStream("/serialization/LargeLanguage.json");
-        String json = readInputStreamToString(is);
-        JsonSerialization js = SerializationProvider.getStandardJsonSerialization(LionWebVersion.v2023_1);
-        long t0 = System.currentTimeMillis();
-        js.deserializeToNodes(json);
-        long t1 = System.currentTimeMillis();
-        long elapsed = t1 - t0;
-        // Elapsed: 879 ms
-        System.out.println("Elapsed: " + elapsed + " ms");
-    }
+  @Test
+  public void deserializeLargeLanguage() {
+    InputStream is = this.getClass().getResourceAsStream("/serialization/LargeLanguage.json");
+    String json = readInputStreamToString(is);
+    JsonSerialization js =
+        SerializationProvider.getStandardJsonSerialization(LionWebVersion.v2023_1);
+    long t0 = System.currentTimeMillis();
+    js.deserializeToNodes(json);
+    long t1 = System.currentTimeMillis();
+    long elapsed = t1 - t0;
+    // Elapsed: 879 ms
+    System.out.println("Elapsed: " + elapsed + " ms");
+  }
 
-    @Test
-    public void serializeLargeLanguage() {
-        InputStream is = this.getClass().getResourceAsStream("/serialization/LargeLanguage.json");
-        String json = readInputStreamToString(is);
-        JsonSerialization js = SerializationProvider.getStandardJsonSerialization(LionWebVersion.v2023_1);
-        List<Node> roots = js.deserializeToNodes(json).stream().filter(n -> n.getParent() == null).collect(Collectors.toList());
-        assertEquals(2, roots.size());
+  @Test
+  public void serializeLargeLanguage() {
+    InputStream is = this.getClass().getResourceAsStream("/serialization/LargeLanguage.json");
+    String json = readInputStreamToString(is);
+    JsonSerialization js =
+        SerializationProvider.getStandardJsonSerialization(LionWebVersion.v2023_1);
+    List<Node> roots =
+        js.deserializeToNodes(json).stream()
+            .filter(n -> n.getParent() == null)
+            .collect(Collectors.toList());
+    assertEquals(2, roots.size());
 
-        // Let's create a separate JsonSerialization, just in case some caches could affect the result
-        js = SerializationProvider.getStandardJsonSerialization(LionWebVersion.v2023_1);
-        long t0 = System.currentTimeMillis();
-        js.serializeTreesToJsonElement(roots.get(0), roots.get(1));
-        long t1 = System.currentTimeMillis();
-        long elapsed = t1 - t0;
-        // Elapsed: 6387 ms
-        System.out.println("Elapsed: " + elapsed + " ms");
-    }
+    // Let's create a separate JsonSerialization, just in case some caches could affect the result
+    js = SerializationProvider.getStandardJsonSerialization(LionWebVersion.v2023_1);
+    long t0 = System.currentTimeMillis();
+    js.serializeTreesToJsonElement(roots.get(0), roots.get(1));
+    long t1 = System.currentTimeMillis();
+    long elapsed = t1 - t0;
+    // Elapsed: 6387 ms
+    System.out.println("Elapsed: " + elapsed + " ms");
+  }
 
-    private String readInputStreamToString(InputStream inputStream) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            return reader.lines().collect(Collectors.joining("\n"));
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+  private String readInputStreamToString(InputStream inputStream) {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+      return reader.lines().collect(Collectors.joining("\n"));
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
     }
+  }
 }
