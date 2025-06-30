a49f0f29c60d73191362755682d1a6bffb5f464d
Federico Tomassetti <federico@strumenta.com>
2025-06-30 14:30:29 +0200

    Removing all references to the old "Metamodel" name in LanguageTest, making them references to "Language" instead

--- core/src/test/java/io/lionweb/language/LanguageTest.java
+++ core/src/test/java/io/lionweb/language/LanguageTest.java
@@ -11,34 +11,34 @@ public class LanguageTest {
 
   @Test
   public void addDependency() {
-    Language m1 = new Language("m1");
-    Language m2 = new Language("m2");
-    Language m3 = new Language("m3");
+    Language l1 = new Language("l1");
+    Language l2 = new Language("l2");
+    Language l3 = new Language("l3");
 
-    assertEquals(Collections.emptyList(), m1.dependsOn());
-    m1.addDependency(m2);
-    assertEquals(Arrays.asList(m2), m1.dependsOn());
-    m1.addDependency(m3);
-    assertEquals(Arrays.asList(m2, m3), m1.dependsOn());
+    assertEquals(Collections.emptyList(), l1.dependsOn());
+    l1.addDependency(l2);
+    assertEquals(Arrays.asList(l2), l1.dependsOn());
+    l1.addDependency(l3);
+    assertEquals(Arrays.asList(l2, l3), l1.dependsOn());
   }
 
   @Test
   public void getAnnotationByName() {
-    Language m1 = new Language("m1");
+    Language l1 = new Language("l1");
 
-    Language m2 = new Language("m2");
-    Annotation a1inM2 = new Annotation(m2, "A1");
+    Language l2 = new Language("l2");
+    Annotation a1inM2 = new Annotation(l2, "A1");
 
-    Language m3 = new Language("m3");
-    Annotation a2inM3 = new Annotation(m3, "A2");
+    Language l3 = new Language("l3");
+    Annotation a2inM3 = new Annotation(l3, "A2");
 
-    assertNull(m1.getAnnotationByName("A1"));
-    assertNull(m1.getAnnotationByName("A2"));
+    assertNull(l1.getAnnotationByName("A1"));
+    assertNull(l1.getAnnotationByName("A2"));
 
-    assertEquals(a1inM2, m2.getAnnotationByName("A1"));
-    assertNull(m2.getAnnotationByName("A2"));
+    assertEquals(a1inM2, l2.getAnnotationByName("A1"));
+    assertNull(l2.getAnnotationByName("A2"));
 
-    assertNull(m3.getAnnotationByName("A1"));
-    assertEquals(a2inM3, m3.getAnnotationByName("A2"));
+    assertNull(l3.getAnnotationByName("A1"));
+    assertEquals(a2inM3, l3.getAnnotationByName("A2"));
   }
 }
