84e6694113397d05eaa2f2d9980fd755dc43c8bc
Federico Tomassetti <federico@strumenta.com>
2025-07-01 11:18:42 +0200

    Mark INamed.getName as Nullable
    # Conflicts:
    #	core/src/main/java/io/lionweb/language/Language.java

--- core/src/main/java/io/lionweb/language/INamed.java
+++ core/src/main/java/io/lionweb/language/INamed.java
@@ -1,5 +1,13 @@
 package io.lionweb.language;
 
+import javax.annotation.Nullable;
+
 public interface INamed {
+  /**
+   * This value may be null either because the element can have an optional name or because
+   * something that should be named is an incorrect state (perhaps because it is being loaded). In
+   * both cases the name could appear null.
+   */
+  @Nullable
   String getName();
 }
