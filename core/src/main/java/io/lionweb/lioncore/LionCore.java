2b6a4c3bd107b9748d8013d09a7a163ac4c93dc2
ftomassetti <federico@strumenta.com>
2025-07-06 16:31:42 +0200

    Update based on the resolution of https://github.com/LionWeb-io/specification/issues/380

--- core/src/main/java/io/lionweb/lioncore/LionCore.java
+++ core/src/main/java/io/lionweb/lioncore/LionCore.java
@@ -171,7 +171,7 @@ public class LionCore {
     }
     if (!INSTANCES.containsKey(lionWebVersion)) {
       final Language instance = new Language(lionWebVersion, "LionCore_M3");
-      // instance.addDependency(LionCoreBuiltins.getInstance(lionWebVersion));
+      // Note the LionCore does not depend on LionCore Builtins, despite using elements from it
       instance.setID("-id-LionCore-M3" + versionIDSuffix);
       instance.setKey("LionCore-M3");
       instance.setVersion(lionWebVersion.getVersionString());
