f65d27238ed66eb73a14e4f6fcca58fad2ceb064
ftomassetti <federico@strumenta.com>
2025-08-02 14:57:06 +0200

    Adding DeltaClient and Participation

---
+++ core/src/main/java/io/lionweb/delta/DeltaClient.java
@@ -0,0 +1,10 @@
+package io.lionweb.delta;
+
+import java.net.URL;
+
+public class DeltaClient {
+
+    public Participation register(URL url, String repositoryName) {
+
+    }
+}
---
+++ core/src/main/java/io/lionweb/delta/Participation.java
@@ -0,0 +1,11 @@
+package io.lionweb.delta;
+
+public class Participation {
+    /**
+     *
+     * @return
+     */
+    public String id() {
+        throw new UnsupportedOperationException();
+    }
+}
