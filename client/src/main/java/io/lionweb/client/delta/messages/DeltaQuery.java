89f472af5c07ed481cece4933ea85c9c7606b1db
ftomassetti <federico@strumenta.com>
2025-08-02 18:09:37 +0200

    Adding more queries

--- client/src/main/java/io/lionweb/client/delta/messages/DeltaQuery.java
+++ client/src/main/java/io/lionweb/client/delta/messages/DeltaQuery.java
@@ -1,8 +1,15 @@
 package io.lionweb.client.delta.messages;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public abstract class DeltaQuery {
-    private String queryId;
-    private List<ProtocolMessage> protocolMessages;
+    public final @NotNull String queryId;
+    public final List<ProtocolMessage> protocolMessages = new LinkedList<>();
+
+    public DeltaQuery(@NotNull String queryId) {
+        this.queryId = queryId;
+    }
 }
