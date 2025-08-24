d8b1ad3771e5b67c3242249df3cc0ab32867bd60
ftomassetti <federico@strumenta.com>
2025-08-24 09:05:52 +0200

    Testing ProxyNode

--- core/src/test/java/io/lionweb/model/impl/AbstractNodeTest.java
+++ core/src/test/java/io/lionweb/model/impl/AbstractNodeTest.java
@@ -1,31 +1,26 @@
 package io.lionweb.model.impl;
 
-import io.lionweb.language.*;
-import io.lionweb.lioncore.LionCore;
-import io.lionweb.model.AnnotationInstance;
-import io.lionweb.model.MockPartitionObserver;
-import io.lionweb.model.ReferenceValue;
-import io.lionweb.serialization.Book;
-import io.lionweb.serialization.Library;
-import org.junit.Test;
-
-import java.util.Arrays;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+
+import io.lionweb.language.*;
+import io.lionweb.model.MockPartitionObserver;
+import org.junit.Test;
 
 public class AbstractNodeTest {
 
   @Test
   public void partitionObserversCanBeAssignedOnlyToRootNodes() {
-      Language language = new Language();
-      language.setID("l1");
-      Concept concept = new Concept();
-      concept.setID("c1");
-      language.addElement(concept);
+    Language language = new Language();
+    language.setID("l1");
+    Concept concept = new Concept();
+    concept.setID("c1");
+    language.addElement(concept);
 
-      assertThrows(UnsupportedOperationException.class, () -> concept.registerPartitionObserver(new MockPartitionObserver()));
-      assertThrows(UnsupportedOperationException.class, () -> concept.unregisterPartitionObserver(new MockPartitionObserver()));
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> concept.registerPartitionObserver(new MockPartitionObserver()));
+    assertThrows(
+        UnsupportedOperationException.class,
+        () -> concept.unregisterPartitionObserver(new MockPartitionObserver()));
   }
-
 }
