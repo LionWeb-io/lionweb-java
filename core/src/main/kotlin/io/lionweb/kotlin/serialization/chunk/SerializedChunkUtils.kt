2e065a1dc5c2c058186ab290f0f59dfa8400eae0
Federico Tomassetti <federico@strumenta.com>
2025-09-12 18:04:28 +0200

    Refactor `SerializedChunk` to `SerializationChunk` and update related methods, tests, and dependencies. Update Gradle version to `0.6.0-SNAPSHOT`.

--- core/src/main/kotlin/io/lionweb/kotlin/serialization/chunk/SerializedChunkUtils.kt
+++ core/src/main/kotlin/io/lionweb/kotlin/serialization/chunk/SerializedChunkUtils.kt
@@ -1,21 +1,58 @@
 package io.lionweb.kotlin.serialization.chunk
 
-import io.lionweb.serialization.data.SerializedChunk
+import io.lionweb.language.Concept
+import io.lionweb.serialization.data.MetaPointer
+import io.lionweb.serialization.data.SerializationChunk
 import io.lionweb.serialization.data.SerializedClassifierInstance
+import io.lionweb.serialization.data.SerializedReferenceValue
 import java.lang.IllegalStateException
 
 /**
  * The first root node within the given SerializedChunk.
  */
-val SerializedChunk.root: SerializedClassifierInstance
+val SerializationChunk.root: SerializedClassifierInstance
     get() = this.classifierInstances.find { it.parentNodeID == null } ?: throw IllegalStateException()
 
 /**
  * Combine this and the received SerializedChunk into a single SerializedChunk containing all nodes.
  * It changes the original SerializedChunk and return it.
  */
-fun SerializedChunk.combine(other: SerializedChunk): SerializedChunk {
+fun SerializationChunk.combine(other: SerializationChunk): SerializationChunk {
     other.classifierInstances.forEach { addClassifierInstance(it) }
     populateUsedLanguages()
     return this
+}
+
+fun SerializedClassifierInstance.getReferenceValues(
+    concept: Concept,
+    referenceName: String,
+): List<SerializedReferenceValue.Entry> = this.getReferenceValues(MetaPointer.from(concept.getReferenceByName(referenceName)))
+
+fun SerializedClassifierInstance.getReferenceValuesIDs(
+    concept: Concept,
+    referenceName: String,
+): List<String?> =
+    this.getReferenceValues(concept, referenceName).map {
+        it.reference
+    }
+
+fun SerializationChunk.getReferenceValues(
+    concept: Concept,
+    referenceName: String,
+): List<SerializedReferenceValue.Entry> = this.root.getReferenceValues(MetaPointer.from(concept.getReferenceByName(referenceName)))
+
+fun SerializationChunk.getReferenceValuesIDs(
+    concept: Concept,
+    referenceName: String,
+): List<String?> = this.root.getReferenceValuesIDs(concept, referenceName)
+
+fun SerializationChunk.combineTree(
+    otherChunk: SerializationChunk,
+    subtreeRoot: SerializedClassifierInstance,
+) {
+    this.addClassifierInstance(subtreeRoot)
+    subtreeRoot.children.forEach { childId ->
+        val child = otherChunk.classifierInstancesByID[childId]!!
+        combineTree(otherChunk, child)
+    }
 }
