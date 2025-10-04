52ad63a75c2569a97c0d6020fcb10a43460f34ee
ftomassetti <federico@strumenta.com>
2025-10-04 17:33:59 +0200

    Introduce containment and multiplicity handling in classifiers, enhance `Annotation` with method chaining, and add related tests.

---
+++ core/src/main/java/io/lionweb/language/Multiplicity.java
@@ -0,0 +1,43 @@
+package io.lionweb.language;
+
+/**
+ * Represents the multiplicity of a feature.
+ *
+ * <p>The {@code Multiplicity} enum includes the following constants: - {@code OPTIONAL}: Indicates
+ * an optional relationship or property that is not required and does not allow multiple instances
+ * (i.e., values accepted 0..1). - {@code REQUIRED}: Represents a required, non-optional property or
+ * relationship that does not allow multiple instances (i.e., values accepted 1..1). - {@code
+ * ZERO_OR_MORE}: Allows zero or more instances, making it optional and allowing multiple instances
+ * (i.e., values accepted 0..*). This is not a valid value for properties. - {@code ONE_OR_MORE}:
+ * Specifies that at least one instance is required, but multiple instances are also allowed (i.e.,
+ * values accepted 1..*). This is not a valid value for properties.
+ *
+ * <p>Each {@code Multiplicity} constant is defined by two attributes: - {@code optional}: Boolean
+ * indicating whether the relationship or property is optional. - {@code multiple}: Boolean
+ * indicating whether multiple instances are allowed.
+ *
+ * <p>This enumeration provides methods to access these attributes: - {@link #isOptional()} to check
+ * if the multiplicity is optional. - {@link #isMultiple()} to check if the multiplicity allows
+ * multiple instances.
+ */
+public enum Multiplicity {
+  OPTIONAL(true, false),
+  REQUIRED(false, false),
+  ZERO_OR_MORE(true, true),
+  ONE_OR_MORE(false, true);
+  private boolean optional;
+  private boolean multiple;
+
+  Multiplicity(boolean optional, boolean multiple) {
+    this.optional = optional;
+    this.multiple = multiple;
+  }
+
+  public boolean isOptional() {
+    return optional;
+  }
+
+  public boolean isMultiple() {
+    return multiple;
+  }
+}
--- core/src/test/java/io/lionweb/model/impl/DynamicAnnotationTest.java
+++ core/src/test/java/io/lionweb/model/impl/DynamicAnnotationTest.java
@@ -2,6 +2,9 @@ package io.lionweb.model.impl;
 
 import static org.junit.Assert.*;
 
+import io.lionweb.language.*;
+import io.lionweb.language.assigners.CommonIDAssigners;
+import io.lionweb.language.assigners.CommonKeyAssigners;
 import io.lionweb.model.ClassifierInstanceUtils;
 import io.lionweb.model.Node;
 import io.lionweb.serialization.*;
@@ -20,5 +23,51 @@ public class DynamicAnnotationTest {
     Node retrievedValue1 =
         ClassifierInstanceUtils.getChildrenByContainmentName(myAnnotation, "values").get(0);
     assertEquals(123, ClassifierInstanceUtils.getPropertyValueByName(retrievedValue1, "amount"));
+  }
+
+  @Test
+  public void annotationDescendants() {
+    Language language = new Language("ALanguage");
+    Concept c1 = new Concept(language, "C1");
+    Concept c2 = new Concept(language, "C2");
+    Concept c3 = new Concept(language, "C3");
+    c2.addContainment("mySubC2", c2, Multiplicity.OPTIONAL);
+    Annotation a1 =
+        new Annotation(language, "A1")
+            .setAnnotates(c1)
+            .addContainment("myC2", c2, Multiplicity.ZERO_OR_MORE)
+            .addContainment("myC3", c3, Multiplicity.OPTIONAL);
+    CommonKeyAssigners.qualifiedKeyAssigner.assignKeys(language);
+    CommonIDAssigners.qualifiedIDAssigner.assignIDs(language);
+
+    DynamicNode n1 = new DynamicNode("n1", c1);
+    DynamicAnnotationInstance a1_1 = new DynamicAnnotationInstance("a1_1", a1);
+    a1_1.setAnnotated(n1);
+    DynamicNode n2 = new DynamicNode("n2", c2);
+    DynamicNode n3 = new DynamicNode("n3", c2);
+    ClassifierInstanceUtils.addChild(a1_1, "myC2", n2);
+    ClassifierInstanceUtils.addChild(a1_1, "myC2", n3);
+    DynamicNode n4 = new DynamicNode("n4", c2);
+    ClassifierInstanceUtils.addChild(n3, "mySubC2", n4);
+
+    DynamicNode n5 = new DynamicNode("n5", c3);
+    ClassifierInstanceUtils.addChild(a1_1, "myC3", n5);
+
+    assertEquals(n3, n4.getParent());
+    assertEquals(a1_1, n4.getParent().getParent());
+    assertEquals(n1, n4.getParent().getParent().getParent());
+
+    assertEquals(a1_1, n3.getParent());
+    assertEquals(n1, n3.getParent().getParent());
+
+    assertEquals(a1_1, n2.getParent());
+    assertEquals(n1, n2.getParent().getParent());
+
+    assertEquals(a1_1, n5.getParent());
+    assertEquals(n1, n5.getParent().getParent());
+
+    assertEquals(n1, a1_1.getParent());
+
+    assertNull(n1.getParent());
   }
 }
