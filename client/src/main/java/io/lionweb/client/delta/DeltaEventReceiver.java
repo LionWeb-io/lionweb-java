e326322951ca2d2327fe130fe5955370e757baa2
ftomassetti <federico@strumenta.com>
2025-10-20 20:38:51 +0200

    Introduce new DeltaEvent classes for references, annotations, children, and general events, enhancing immutability and adding comprehensive documentation.

--- client/src/main/java/io/lionweb/client/delta/DeltaEventReceiver.java
+++ client/src/main/java/io/lionweb/client/delta/DeltaEventReceiver.java
@@ -1,8 +1,8 @@
 package io.lionweb.client.delta;
 
-import io.lionweb.client.delta.messages.DeltaEvent;
+import io.lionweb.client.delta.messages.CommonDeltaEvent;
 
 public interface DeltaEventReceiver {
 
-  void receiveEvent(DeltaEvent event);
+  void receiveEvent(CommonDeltaEvent event);
 }
