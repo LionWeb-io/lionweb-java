94daa816e6982b33ba7018a7af44b26a034dd6b0
ftomassetti <federico@strumenta.com>
2025-11-09 15:34:38 +0100

    Introduce `CompanyLanguage` and associated models with serialization and archive handling.
    - Added `CompanyLanguage` definition including `Company`, `Department`, `Employee`, and `Role` concepts.
    - Implemented classes for `Company`, `Department`, `Employee`, and `Role` with their respective properties and behaviors.
    - Enhanced `LionWebArchive` with support for storing and loading serialized chunks.
    - Added comprehensive test cases to validate model serialization and archive functionality.

---
+++ extensions/src/test/java/io/lionweb/archive/Department.java
@@ -0,0 +1,18 @@
+package io.lionweb.archive;
+
+import io.lionweb.language.INamed;
+import io.lionweb.model.ClassifierInstanceUtils;
+import io.lionweb.model.impl.DynamicNode;
+
+public class Department extends DynamicNode implements INamed {
+
+    public Department(String id, String name) {
+        super(id, CompanyLanguage.getDepartment());
+        ClassifierInstanceUtils.setPropertyValueByName(this, "name", name);
+    }
+
+    @Override
+    public String getName() {
+        return (String) ClassifierInstanceUtils.getPropertyValueByName(this, "name");
+    }
+}
---
+++ extensions/src/test/java/io/lionweb/archive/Employee.java
@@ -0,0 +1,35 @@
+package io.lionweb.archive;
+
+import io.lionweb.model.ClassifierInstance;
+import io.lionweb.model.ClassifierInstanceUtils;
+import io.lionweb.model.impl.DynamicNode;
+
+public class Employee extends DynamicNode {
+
+    public Employee(String id, String firstName, String lastName) {
+        super(id, CompanyLanguage.getEmployee());
+        ClassifierInstanceUtils.setPropertyValueByName(this, "first name", firstName);
+        ClassifierInstanceUtils.setPropertyValueByName(this, "last name", lastName);
+    }
+
+    public void setSalary(int salary) {
+        ClassifierInstanceUtils.setPropertyValueByName(this, "salary", salary);
+    }
+
+    public int getSalary() {
+        Object res = ClassifierInstanceUtils.getPropertyValueByName(this, "salary");
+        if (res == null) {
+            return -1;
+        } else {
+            return (int) res;
+        }
+    }
+
+    public void addDepartment(Department department) {
+        ClassifierInstanceUtils.addReferenceByName(this, "departments", department);
+    }
+
+    public void addRole(Role role) {
+        ClassifierInstanceUtils.addReferenceByName(this, "roles", role);
+    }
+}
