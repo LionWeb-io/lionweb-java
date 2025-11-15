349b9df95a3cce15ab6bacd9225fb35788b08116
ftomassetti <federico@strumenta.com>
2025-11-15 16:21:23 +0100

    Update `lionweb-gradle-plugin` to support language generation tasks
    - Implement `generateLWLanguages` task for processing LionWeb language files.
    - Add default configuration for `languagesDirectory` and `generationDirectory`.
    - Introduce functional test for the new task.
    - Include repository, Gson, and Protobuf dependencies in the plugin build script.
    - Integrate sample `library-language.json` for testing language generation logic.

--- gradle-plugin/src/main/java/io/lionweb/gradleplugin/GenerateLanguageTask.java
+++ gradle-plugin/src/main/java/io/lionweb/gradleplugin/GenerateLanguageTask.java
@@ -1,18 +1,74 @@
 package io.lionweb.gradleplugin;
 
+import io.lionweb.LionWebVersion;
+import io.lionweb.language.Language;
+import io.lionweb.serialization.JsonSerialization;
+import io.lionweb.serialization.LowLevelJsonSerialization;
+import io.lionweb.serialization.SerializationProvider;
+import io.lionweb.serialization.data.SerializationChunk;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.file.RegularFile;
 import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.*;
+import org.gradle.internal.impldep.org.apache.commons.codec.language.bm.Lang;
 
-import org.gradle.api.DefaultTask;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class GenerateLanguageTask extends DefaultTask {
-    @Input
-    abstract RegularFileProperty getLanguagesDirectory();
-    @Input
-    abstract RegularFileProperty getGenerationDirectory();
+  @InputFiles
+  @Optional
+  abstract RegularFileProperty getLanguagesDirectory();
 
-    @TaskAction
-    public void run() {
-        getLogger().lifecycle("Java code for LionWeb Languages generated");
-    }
+  @InputFiles
+  @Optional
+  abstract RegularFileProperty getGenerationDirectory();
+
+  @TaskAction
+  public void run() {
+      getLogger().info("GenerateLanguageTask - Starting");
+      File languagesDirectory = getLanguagesDirectory().isPresent() ? getLanguagesDirectory().getAsFile().get() : new java.io.File(getProject().getProjectDir(), "src/main/lionweb");
+      if (!languagesDirectory.exists() || !languagesDirectory.isDirectory()) {
+          getLogger().error("GenerateLanguageTask - Languages directory does not exist or is not a directory");
+          throw new GradleException("Languages directory does not exist or is not a directory");
+      }
+      try (Stream<Path> stream = Files.walk(languagesDirectory.toPath())) {
+          List<Path> files = stream
+                  .filter(Files::isRegularFile)
+                  .filter(f -> f.toString().endsWith(".json") || f.toString().endsWith(".pb"))
+                  .collect(Collectors.toList());
+          if (files.isEmpty()) {
+              getLogger().warn("GenerateLanguageTask - No files found");
+              return;
+          }
+          getLogger().lifecycle("Language files found: " + files.size());
+          // TODO consider topological order of languages
+          files.forEach(f -> {
+              try {
+                  if (f.toString().endsWith(".json")) {
+                      SerializationChunk chunk = new LowLevelJsonSerialization().deserializeSerializationBlock(f.toFile());
+                      LionWebVersion lionWebVersion = LionWebVersion.fromValue(chunk.getSerializationFormatVersion());
+                      JsonSerialization serialization = SerializationProvider.getStandardJsonSerialization(lionWebVersion);
+                      Language language = serialization.loadLanguage(f.toFile());
+                      getLogger().lifecycle("LionWeb Language loaded: " + language.getName());
+                  } else if (f.toString().endsWith(".pb")) {
+
+                  } else {
+                      throw new UnsupportedOperationException("Unsupported file extension: <" + f.toString() + ">");
+                  }
+              }catch (IOException e) {
+                  throw new RuntimeException(e);
+              }
+          });
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+      getLogger().lifecycle("Java code for LionWeb Languages generated");
+  }
 }
--- gradle-plugin/src/main/java/io/lionweb/gradleplugin/LionWebPlugin.java
+++ gradle-plugin/src/main/java/io/lionweb/gradleplugin/LionWebPlugin.java
@@ -4,16 +4,29 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class LionWebPlugin implements Plugin<Project> {
-    @Override
-    public void apply(Project project) {
-        // Create extension and set sensible defaults (conventions)
-        LionWebPluginExtension ext = project.getExtensions().create("lionweb", LionWebPluginExtension.class);
-        // TODO configure
-
-        // Register task and map extension -> task inputs (lazy wiring)
-        project.getTasks().register("generateLWLanguages", GenerateLanguageTask.class, task -> {
-            task.setGroup("lionweb");
-            task.setDescription("Generates LionWeb languages");
-        });
+  @Override
+  public void apply(Project project) {
+      project.getLogger().info("Applying LionWeb plugin");
+    // Create extension and set sensible defaults (conventions)
+    LionWebPluginExtension ext =
+        project.getExtensions().create("lionweb", LionWebPluginExtension.class);
+    if (!ext.getLanguagesDirectory().isPresent()) {
+        ext.getLanguagesDirectory().set(new java.io.File(project.getProjectDir(), "src/main/lionweb"));
     }
+    if (!ext.getGenerationDirectory().isPresent()) {
+        ext.getGenerationDirectory().set(new java.io.File(project.getLayout().getBuildDirectory().get().getAsFile(), "generated-lionweb"));
+    }
+    // TODO configure
+
+    // Register task and map extension -> task inputs (lazy wiring)
+    project
+        .getTasks()
+        .register(
+            "generateLWLanguages",
+            GenerateLanguageTask.class,
+            task -> {
+              task.setGroup("lionweb");
+              task.setDescription("Generates LionWeb languages");
+            });
+  }
 }
