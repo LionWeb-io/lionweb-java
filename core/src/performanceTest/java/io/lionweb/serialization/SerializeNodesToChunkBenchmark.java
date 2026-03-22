e3ad08a4c8e1bcb58e1ed5e9425ddfbde7ed6f57
ftomassetti <federico@strumenta.com>
2026-03-22 12:48:42 +0100

    Update JMH benchmark configuration: increase warmup and measurement iterations, and add an additional fork.

--- core/src/performanceTest/java/io/lionweb/serialization/SerializeNodesToChunkBenchmark.java
+++ core/src/performanceTest/java/io/lionweb/serialization/SerializeNodesToChunkBenchmark.java
@@ -45,9 +45,9 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
  */
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
-@Fork(1)
+@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(2)
 @State(Scope.Thread)
 public class SerializeNodesToChunkBenchmark {
 
