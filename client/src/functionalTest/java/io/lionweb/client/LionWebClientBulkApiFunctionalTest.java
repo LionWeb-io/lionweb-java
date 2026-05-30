0eae8b33bf41a4be0161fbd02c5374992793ffb0
ftomassetti <federico@strumenta.com>
2026-05-30 10:24:18 +0200

    Update functional tests to replace `CommonChecks` with `IdUtils` for ID validation

--- client/src/functionalTest/java/io/lionweb/client/LionWebClientBulkApiFunctionalTest.java
+++ client/src/functionalTest/java/io/lionweb/client/LionWebClientBulkApiFunctionalTest.java
@@ -14,7 +14,7 @@ import io.lionweb.model.Node;
 import io.lionweb.model.impl.DynamicNode;
 import io.lionweb.serialization.data.SerializationChunk;
 import io.lionweb.serialization.data.SerializedClassifierInstance;
-import io.lionweb.utils.CommonChecks;
+import io.lionweb.utils.IdUtils;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
@@ -158,14 +158,14 @@ public class LionWebClientBulkApiFunctionalTest extends AbstractClientFunctional
 
     List<String> ids1 = client.ids(78);
     assertEquals(78, ids1.size());
-    assertTrue(ids1.stream().allMatch(CommonChecks::isValidID));
+    assertTrue(ids1.stream().allMatch(IdUtils::isValidID));
 
     List<String> ids2 = client.ids(0);
     assertEquals(0, ids2.size());
 
     List<String> ids3 = client.ids(1);
     assertEquals(1, ids3.size());
-    assertTrue(ids3.stream().allMatch(CommonChecks::isValidID));
+    assertTrue(ids3.stream().allMatch(IdUtils::isValidID));
   }
 
   @Test
