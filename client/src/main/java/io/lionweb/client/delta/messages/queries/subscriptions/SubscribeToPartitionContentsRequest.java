968a89d7b5d9d2f7a73cfe02e687af207ff7b438
ftomassetti <federico@strumenta.com>
2026-05-30 19:03:03 +0200

    Make `partition` field final in `SubscribeToPartitionContentsRequest`.

--- client/src/main/java/io/lionweb/client/delta/messages/queries/subscriptions/SubscribeToPartitionContentsRequest.java
+++ client/src/main/java/io/lionweb/client/delta/messages/queries/subscriptions/SubscribeToPartitionContentsRequest.java
@@ -4,10 +4,15 @@ import io.lionweb.client.delta.messages.DeltaQuery;
 import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a request to subscribe to the contents of a specific partition.
+ * This class allows a client to request events pertaining to a given partition
+ * identified by its node ID.
+ */
 public class SubscribeToPartitionContentsRequest extends DeltaQuery {
 
   /** Node id of the partition this client wants to receive events of. */
-  public @NotNull String partition;
+  public final @NotNull String partition;
 
   public SubscribeToPartitionContentsRequest(@NotNull String queryId, @NotNull String partition) {
     super(queryId);
